b48853e4d5c03b09ab0c35e4e4fbd1563f0fc27f
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2025-08-28 16:58:16 +0900

    feat: 뱃지 모달 스타일 수정

--- components/Social/ShareBadgeModal.tsx
+++ components/Social/ShareBadgeModal.tsx
@@ -1,4 +1,6 @@
-import { Modal, View, Text, TouchableOpacity, Image } from 'react-native';
+import { Modal, View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { useState } from 'react';
+import Octicons from '@expo/vector-icons/Octicons';
 
 interface Badge {
   icon: string;
@@ -19,32 +21,38 @@ export default function ShareBadgeModal({
   onClose,
   onSubmit,
 }: ShareBadgeModalProps) {
+  const [content, setContent] = useState('');
+
   return (
     <Modal visible={visible} transparent animationType="fade" onRequestClose={onClose}>
       <TouchableOpacity className="flex-1 bg-black/50" activeOpacity={1} onPress={onClose}>
-        <View className="flex-1 items-center justify-center">
-          <View className="mx-4 rounded-3xl bg-white p-5 shadow-lg" style={{ minWidth: 320 }}>
-            <View className="mb-1 flex-row items-center justify-center">
-              <Text className="pt-4 text-xl font-extrabold text-gray-800">나의 뱃지 공유하기</Text>
-              <Image
-                source={require('../../assets/upload.png')}
-                className="ml-2 mt-3 h-4 w-4"
-                resizeMode="contain"
-              />
+        <View className="mx-10 flex-1 items-center justify-center">
+          <View
+            className="flex items-center rounded-3xl bg-white p-6 shadow-lg"
+            style={{ minWidth: 320 }}>
+            <View className="my-1 flex-row items-center justify-center text-gray-800">
+              <Text className="mr-1 text-lg font-bold">나의 뱃지 공유하기</Text>
+              <Octicons name="pencil" size={18} />
             </View>
 
             <View className="mb-6 items-center">
-              <Text className="text-sm text-gray-600">
+              <Text className="text-xs text-gray-500">
                 나의 성과를 소셜에 올려 사람들과 소통해보세요!
               </Text>
             </View>
 
-            <View className="mb-6 rounded-lg border border-gray-200 bg-gray-100 px-3 py-4">
-              <Text className="mb-2 text-gray-400">이 뱃지를 얻기 위해 무슨 노력을 하셨나요?</Text>
-              <Text className="mb-8 text-gray-400">(최대 100자)</Text>
+            <View className="mb-6 flex h-[230px] w-[270px] justify-between rounded-lg border border-gray-200 bg-gray-100 px-2 py-3">
+              <TextInput
+                className="rounded-lg bg-gray-100 p-3 text-xs text-gray-800"
+                placeholder={`이 뱃지를 얻기 위해 무슨 노력을 하셨나요?\n(최대 100자)`}
+                maxLength={100}
+                multiline
+                value={content}
+                onChangeText={setContent}
+              />
 
-              <View className="mt-4 rounded-lg bg-white p-4">
-                <Text className="mb-3 text-base font-medium text-gray-800">획득한 뱃지</Text>
+              <View className="mt-6 flex rounded-lg bg-white p-4">
+                <Text className="mb-3 text-sm font-medium text-gray-800">획득한 뱃지</Text>
                 <View className="flex-row items-center">
                   <View className="mr-4 h-12 w-12 items-center justify-center rounded-full border border-gray-200 bg-white">
                     <Text className="text-2xl">{badge?.icon}</Text>
@@ -57,12 +65,18 @@ export default function ShareBadgeModal({
               </View>
             </View>
 
+            {/* 버튼 */}
             <View className="flex-row space-x-3">
               <TouchableOpacity className="flex-1 rounded-lg py-1" onPress={onClose}>
-                <Text className="text-center text-xl font-extrabold text-[#007AFF]">취소</Text>
+                <Text className="text-center text-lg font-bold text-[#888]">취소</Text>
               </TouchableOpacity>
-              <TouchableOpacity className="flex-1 rounded-lg py-1" onPress={() => onSubmit('')}>
-                <Text className="text-center text-xl font-extrabold text-[#007AFF]">작성하기</Text>
+              <TouchableOpacity
+                className="flex-1 rounded-lg py-1"
+                onPress={() => {
+                  onSubmit(content);
+                  setContent('');
+                }}>
+                <Text className="text-center text-lg font-bold text-[#8953E0]">작성하기</Text>
               </TouchableOpacity>
             </View>
           </View>
