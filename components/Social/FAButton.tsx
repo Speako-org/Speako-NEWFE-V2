893fef71ed0faa657d934281a9ba9761b5830bf8
hyeyoon23 <109073947+hyeyoon23@users.noreply.github.com>
2025-09-07 23:35:40 +0900

    feat: 소셜페이지 플로팅버튼 그림자값 조정

--- components/Social/FAButton.tsx
+++ components/Social/FAButton.tsx
@@ -8,7 +8,7 @@ interface FAButtonProps {
 export default function FAButton({ onPress }: FAButtonProps) {
   return (
     <TouchableOpacity
-      className="absolute bottom-36 right-6 h-14 w-14 items-center justify-center rounded-full bg-white shadow-md"
+      className="absolute bottom-40 right-9 h-14 w-14 items-center justify-center rounded-full bg-white shadow shadow-stone-400"
       onPress={onPress}>
       <Octicons name="pencil" size={24} color="#8953E0" />
     </TouchableOpacity>
